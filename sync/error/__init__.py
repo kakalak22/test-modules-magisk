21265f62b2d8e785f681d02f1de9ea8612104606
bao-tran-saltovn <124113788+bao-tran-saltovn@users.noreply.github.com>
2023-12-04 20:50:44 +0700

    update

--- sync/error/__init__.py
+++
@@ -1,9 +0,0 @@
-from .ConfigError import ConfigError
-from .MagiskModuleError import MagiskModuleError
-from .Result import Result
-
-__all__ = [
-    "ConfigError",
-    "MagiskModuleError",
-    "Result"
-]
